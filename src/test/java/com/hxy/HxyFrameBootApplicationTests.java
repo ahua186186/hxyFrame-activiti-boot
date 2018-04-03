44b07d2a53057815ff2b07b36ea5c0d4c6ec56dc
jason <270847975@qq.com>
2018-04-03 18:39:26 +0800

    test

--- src/test/java/com/hxy/HxyFrameBootApplicationTests.java
+++ src/test/java/com/hxy/HxyFrameBootApplicationTests.java
@@ -9,8 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class HxyFrameBootApplicationTests {
 
+
 	@Test
 	public void contextLoads() {
+		System.out.println("test");
 	}
 
 }
